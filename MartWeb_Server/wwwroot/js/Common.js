3e31aa9bccf6ea9c39f67b7cbadb6539355a14b6
Chris Norton <officialantiviral@gmail.com>
2022-07-04 21:05:17 -0400

    Added Javascript alerts and toasts

---
+++ MartWeb_Server/wwwroot/js/Common.js
@@ -0,0 +1,24 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type === "info") {
+        toastr.info(message, "Note", { timeOut: 5000 });
+    }
+    if (type === "warning") {
+        toastr.warning(message, "Warning", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failure", { timeOut: 5000 });
+    }
+}
+
+window.ShowAlert = (icon, title) => {
+    
+        Swal.fire({
+            icon: icon,
+            title: title,
+
+        })
+    
+}
