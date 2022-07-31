313a69efd250bb30ab6d524a250fafba80d96d04
Chris Norton <officialantiviral@gmail.com>
2022-07-31 01:08:18 -0400

    fixed checkbox errors and image display

---
+++ MartWeb_Server/ServiceFolder/IService/IFileUpload.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace MartWeb_Server.ServiceFolder.IService
+{
+    public interface IFileUpload
+    {
+        Task<string> UploadFile(IBrowserFile file);
+
+        bool DeleteFile(string filePath);
+    }
+}
