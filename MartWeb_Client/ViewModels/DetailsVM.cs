7ddd51692dec479a52171056d33ed1b134d972a1
Chris Norton <officialantiviral@gmail.com>
2022-08-16 01:40:44 -0400

    select prices and single product

---
+++ MartWeb_Client/ViewModels/DetailsVM.cs
@@ -0,0 +1,20 @@
+﻿using Mart_Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MartWeb_Client.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            ProductPrice = new();
+            Count = 1;
+        }
+
+        [Range(1, int.MaxValue, ErrorMessage ="Value must be greater than 0")]
+        public int Count { get; set; }
+        [Required]
+        public int SelectedProductPriceId { get; set; }
+        public ProductPriceDTO ProductPrice { get; set; }
+    }
+}
--- Mart_API/Controllers/ProductController.cs
+++ Mart_API/Controllers/ProductController.cs
@@ -33,7 +33,7 @@ namespace Mart_API.Controllers
                 });                
             }
 
-            var product = _productRepository.Get(productId.Value);
+            var product = await _productRepository.Get(productId.Value);
             if(product == null)
             {
                 return BadRequest(new ErrorModelDTO()
